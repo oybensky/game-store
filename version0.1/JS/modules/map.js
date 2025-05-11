788c0af14ef38fcc63b9a01049669800eb933609
oybensky <lenskybensky@gmail.com>
2025-05-10 23:17:11 -0400

    little map update

--- version0.1/JS/modules/map.js
+++ version0.1/JS/modules/map.js
@@ -1,9 +1,33 @@
-export function initMap() {
+export async function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 45.45056521648996, lng: -73.6141017240869 },
         zoom: 14,
         mapId: "db641b33843059ef"
     });
+
+    try{
+        fetch("places.json")
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(location => {
+                    const marker = new google.maps.Marker({
+                        position: { lat: location.latitude, lng: location.longitude },
+                        map: map,
+                        title: location.name
+                    });
+
+                    const infoWindow = new google.maps.InfoWindow({
+                        content: `<h1>${location.name}</h1><p>${location.description}</p>`
+                    });
+
+                    marker.addListener("click", () => {
+                        infoWindow.open(map, marker);
+                    });
+                });
+            });
+    } catch (error) {
+        console.error("Error fetching places.json:", error);
+    }
 
     const marker = new google.maps.Marker({
         position: { lat: 45.45056521648996, lng: -73.6141017240869 },
